ad2e51820818404d7a9bb4f7a31b6b7eca1ca5fa
playbooks <eric@erichubbell.com>
2025-06-12 15:18:35 -0600

    config updates

--- vite.config.ts
+++ vite.config.ts
@@ -8,30 +8,34 @@ export function pushBuild() {
 		closeBundle: async () => {
 			exec('dts-bundle-generator --config dts.config.ts', (response, error) => {
 				if (error) console.error(error);
-				// console.log(response);
+				if (response) console.log(response);
 				exec('npx yalc push', (response, error) => (error ? console.error(error) : null));
 			});
 		},
 	};
 }
 
-export default defineConfig({
-	base: './',
-	build: {
-		lib: {
-			entry: path.resolve(__dirname, 'src/index.ts'),
-			name: 'Serializes',
-			formats: ['es', 'cjs'],
-			fileName: format => `index.${format}.js`,
+export default defineConfig(({ mode }) => {
+	const plugins = mode !== 'production' ? [pushBuild()] : [];
+
+	return {
+		base: './',
+		build: {
+			lib: {
+				entry: path.resolve(__dirname, 'src/index.ts'),
+				name: 'Serializes',
+				formats: ['es', 'cjs'],
+				fileName: format => `index.${format}.js`,
+			},
 		},
-	},
-	plugins: [pushBuild()],
-	resolve: {
-		alias: {
-			src: path.resolve(__dirname, '/src'),
-			normalizers: path.resolve(__dirname, '/src/normalizers'),
-			serializers: path.resolve(__dirname, '/src/serializers'),
-			utils: path.resolve(__dirname, '/src/utils'),
+		plugins,
+		resolve: {
+			alias: {
+				src: path.resolve(__dirname, '/src'),
+				normalizers: path.resolve(__dirname, '/src/normalizers'),
+				serializers: path.resolve(__dirname, '/src/serializers'),
+				utils: path.resolve(__dirname, '/src/utils'),
+			},
 		},
-	},
+	};
 });
